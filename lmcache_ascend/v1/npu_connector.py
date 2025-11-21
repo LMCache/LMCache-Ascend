[bugfix] Fix precision issues on Ascend 310p

lmcache_ascend/v1/npu_connector.py
@@ -13,6 +13,7 @@ from lmcache.v1.gpu_connector import (
     VLLMPagedMemLayerwiseGPUConnector,
 )
 from lmcache.v1.memory_management import MemoryFormat, MemoryObj
+from lmcache_ascend.v1.memory_management import is_310p
 import lmcache_ascend.c_ops as lmc_ops
 
 logger = init_logger(__name__)
@@ -21,6 +22,60 @@ class VLLMBufferLayerwiseNPUConnector(VLLMBufferLayerwiseGPUConnector):
     pass
 
 class VLLMPagedMemNPUConnectorV2(VLLMPagedMemGPUConnectorV2):
+    def __init__(
+        self,
+        hidden_dim_size: int,
+        num_layers: int,
+        use_gpu: bool = False,
+        **kwargs,
+    ):
+        """
+        If use_gpu is true, it will create a gpu intermediate buffer. In this
+        case, it requires the following kwargs:
+        - chunk_size: The MAX size of the chunk to be copied to GPU.
+        - dtype: The data type of the intermediate buffer.
+        """
+        self.hidden_dim_size = hidden_dim_size
+        self.num_layers = num_layers
+        self.kv_cache_pointers = torch.empty(
+            num_layers, dtype=torch.int64, device="cpu"
+        )
+        # Not sure we need a dict here. Maybe a single GPU connector always
+        # works with a single device?
+        self.kv_cache_pointers_on_gpu: dict[int, torch.Tensor] = {}
+        self.page_buffer_size = 0
+
+        self.kvcaches: Optional[List[torch.Tensor]] = None
+
+        self.gpu_buffer: Optional[torch.Tensor] = None
+        self.use_mla = "use_mla" in kwargs and kwargs["use_mla"]
+        if is_310p():
+            assert "num_kv_head" in kwargs, (
+                "num_kv_head should be provided in 310p",
+            )
+            assert "head_size" in kwargs, (
+                "head_size should be provided in 310p",
+            )
+            self.num_kv_head = kwargs["num_kv_head"]
+            self.head_size = kwargs["head_size"]
+        if use_gpu:
+            assert "chunk_size" in kwargs, (
+                "chunk_size should be provided to create a GPU buffer."
+            )
+            assert "dtype" in kwargs, "dtype should be provided to create a GPU buffer."
+            assert "device" in kwargs, (
+                "device should be provided to create a GPU buffer."
+            )
+            shape = self.get_shape(kwargs["chunk_size"])
+            self.dtype = kwargs["dtype"]
+            self.device = kwargs["device"]
+            self.gpu_buffer = torch.empty(
+                shape, dtype=kwargs["dtype"], device=kwargs["device"]
+            )
+
+        self.store_stream = torch.cuda.Stream()
+        self.load_stream = torch.cuda.Stream()
+
     def _initialize_pointers(self, kv_caches: List[torch.Tensor]) -> torch.Tensor:
         self.device = kv_caches[0].device
         assert self.device.type == "npu", "The device should be Ascend NPU."
@@ -38,10 +93,155 @@ class VLLMPagedMemNPUConnectorV2(VLLMPagedMemGPUConnectorV2):
             self.page_buffer_size = kv_caches[0].shape[-3] * kv_caches[0].shape[-2]
         else:
             # kv_caches[0].shape: [2, num_pages, page_size, num_heads, head_size]
+            # 310P: [2, num_blocks, num_kv_heads * head_size // 16, block_size, 16]
+            # 910B: [2, num_blocks, block_size, num_kv_heads, head_size]
             assert kv_caches[0].dim() == 5
-            self.page_buffer_size = kv_caches[0].shape[1] * kv_caches[0].shape[2]
+            if is_310p():
+                self.block_size = kv_caches[0].shape[3]
+                self.page_buffer_size = kv_caches[0].shape[1] * self.block_size
+            else:
+                self.page_buffer_size = kv_caches[0].shape[1] * kv_caches[0].shape[2]
 
         return self.kv_cache_pointers_on_gpu[idx]
+
+    def to_gpu_310p(self, memory_obj: MemoryObj, start: int, end: int, **kwargs):
+        """Expect a kwarg 'kvcaches' which is a nested tuple of K and V tensors.
+        The kvcaches should correspond to the "WHOLE token sequence".
+
+        Note:
+          1. This function expects the 'slot_mapping' is a "full slot mapping"
+             where it's length is the same as the whole token sequence.
+          2. In the case that there is prefix caching, slot_mapping will starts
+             with -1s until the end of the matched prefix. The start and end
+             should NEVER overlap with the prefix caching (which means the
+             underlying CUDA kernel will never see -1 in slot_mapping)
+
+
+        :raises ValueError: If 'kvcaches' is not provided in kwargs.
+        :raises AssertionError: If the memory object does not have a tensor.
+        :raises ValueError: If 'slot_mapping' is not provided in kwargs.
+        """
+        assert memory_obj.tensor is not None
+
+        self.initialize_kvcaches_ptr(**kwargs)
+
+        assert self.kvcaches is not None, (
+            "kvcaches should be provided in kwargs or initialized beforehand."
+        )
+
+        if self.use_mla:
+            if memory_obj.metadata.fmt != MemoryFormat.KV_MLA_FMT:
+                raise ValueError(
+                    "The memory object should be in KV_MLA_FMT format in"
+                    " order to be processed by VLLMPagedMemGPUConnector"
+                )
+        else:
+            if memory_obj.metadata.fmt != MemoryFormat.KV_2LTD:
+                raise ValueError(
+                    "The memory object should be in KV_2LTD format in"
+                    " order to be processed by VLLMPagedMemGPUConnector"
+                )
+
+        if "slot_mapping" not in kwargs:
+            raise ValueError("'slot_mapping' should be provided in kwargs.")
+
+        slot_mapping: torch.Tensor = kwargs["slot_mapping"]
+
+        kv_cache_pointers = self._initialize_pointers(self.kvcaches)
+
+        tmp_gpu_buffer = torch.empty(
+            memory_obj.tensor.size(), dtype=self.dtype, device=self.device
+        )
+
+        tmp_gpu_buffer.copy_(memory_obj.tensor)
+
+        lmc_ops.multi_layer_kv_transfer_310p(
+            tmp_gpu_buffer,
+            kv_cache_pointers,
+            slot_mapping[start:end],
+            self.kvcaches[0].device,
+            self.page_buffer_size,
+            False,
+            self.use_mla,
+            self.num_kv_head,
+            self.head_size,
+            self.block_size
+        )
+
+    def from_gpu_310p(self, memory_obj: MemoryObj, start: int, end: int, **kwargs):
+        """Expect a kwarg 'kvcaches' which is a nested tuple of K and V tensors.
+        The kvcaches should correspond to the "WHOLE token sequence".
+
+        Will set the memory_obj.metadata.fmt to MemoryFormat.KV_2LTD.
+
+        Note:
+          1. This function expects the 'slot_mapping' is a "full slot mapping"
+             where it's length is the same as the whole token sequence.
+          2. In the case that there is prefix caching, slot_mapping will starts
+             with -1s until the end of the matched prefix. The start and end
+             should NEVER overlap with the prefix caching (which means the
+             underlying CUDA kernel will never see -1 in slot_mapping)
+
+        :raises ValueError: If 'kvcaches' is not provided in kwargs,
+        :raises AssertionError: If the memory object does not have a tensor.
+        :raises ValueError: If 'slot_mapping' is not provided in kwargs.
+        """
+        assert memory_obj.tensor is not None
+
+        self.initialize_kvcaches_ptr(**kwargs)
+        assert self.kvcaches is not None, (
+            "kvcaches should be provided in kwargs or initialized beforehand."
+        )
+
+        if "slot_mapping" not in kwargs:
+            raise ValueError("'slot_mapping' should be provided in kwargs.")
+
+        slot_mapping: torch.Tensor = kwargs["slot_mapping"]
+
+        kv_cache_pointers = self._initialize_pointers(self.kvcaches)
+
+        assert self.gpu_buffer.device == self.kvcaches[0].device
+
+        tmp_gpu_buffer = torch.empty(
+            memory_obj.tensor.size(), dtype=self.dtype, device=self.device
+        )
+
+        lmc_ops.multi_layer_kv_transfer_310p(
+            tmp_gpu_buffer,
+            kv_cache_pointers,
+            slot_mapping[start:end],
+            self.kvcaches[0].device,
+            self.page_buffer_size,
+            True,
+            self.use_mla,
+            self.num_kv_head,
+            self.head_size,
+            self.block_size
+        )
+
+        memory_obj.tensor.copy_(tmp_gpu_buffer)
+        if self.use_mla:
+            memory_obj.metadata.fmt = MemoryFormat.KV_MLA_FMT
+
+    def batched_to_gpu(self, memory_objs, starts, ends, **kwargs):
+        with torch.cuda.stream(self.load_stream):
+            for memory_obj, start, end in zip(memory_objs, starts, ends, strict=False):
+                if is_310p():
+                    self.to_gpu_310p(memory_obj, start, end, **kwargs)
+                else:
+                    self.to_gpu(memory_obj, start, end, **kwargs)
+        self.load_stream.synchronize()
+
+    def batched_from_gpu(self, memory_objs, starts, ends, **kwargs):
+        for memory_obj, start, end in zip(memory_objs, starts, ends, strict=False):
+            if is_310p():
+                self.from_gpu_310p(memory_obj, start, end, **kwargs)
+            else:
+                self.from_gpu(memory_obj, start, end, **kwargs)
+
+    def get_shape(self, num_tokens: int) -> torch.Size:
+        kv_size = 1 if self.use_mla else 2
+        return torch.Size([kv_size, self.num_layers, num_tokens, self.hidden_dim_size])
 
 
 class VLLMPagedMemLayerwiseNPUConnector(VLLMPagedMemLayerwiseGPUConnector):