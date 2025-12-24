fcce45200c1f065ffd82651bf28a2b8aa1988fd3
Matthew Yeung <yyygggfff@hotmail.com>
2025-12-24 15:39:02 +0000

    Fix layerwise syncmode failed due to storage manager not post_inited (#97)
    * Fix layerwise offload in sync mode
    
    ---------
    
    Signed-off-by: Matthew Yeung <yyygggfff@hotmail.com>

--- lmcache_ascend/__init__.py
+++ lmcache_ascend/__init__.py
@@ -7,10 +7,28 @@ if _build_info.__framework_name__ == "pytorch":
     from functools import partial
     import lmcache
 
+    # TODO (gingfung): Currently we patch all the cuda calls due to effort to port all torch.cuda
+    # will disabled torch.jit
+    # NOTE: this must be done early in the patch prior the cacheengine to avoid falling into non_cuda_equivalent
+    from torch_npu.contrib import transfer_to_npu
+
     import lmcache_ascend
     import lmcache_ascend.c_ops as ascend_c_ops
 
     sys.modules["lmcache.c_ops"] = ascend_c_ops
+
+    # The following patches are related for single-layer offload in sync mode i.e. enable_async_loading = False
+    #  in pre LMCache v0.3.9, the sync mode was broken for layerwise due to storage_manager post init as seen here:
+    #  https://github.com/LMCache/LMCache/issues/1794
+    #  https://github.com/LMCache/LMCache/pull/1852
+    #  https://github.com/LMCache/LMCache/pull/1795
+    #  TODO (gingfung): we should remove these once we release v0.3.9
+    from lmcache.v1.storage_backend.storage_manager import StorageManager
+    from lmcache_ascend.v1.storage_backend.storage_manager import post_init_fix as storage_post_init_fix
+    StorageManager.post_init = storage_post_init_fix
+    from lmcache.v1.cache_engine import LMCacheEngine
+    from lmcache_ascend.v1.cache_engine import post_init_fix as cache_engine_post_init_fix
+    LMCacheEngine.post_init = cache_engine_post_init_fix
 
     from lmcache_ascend.integration.vllm.vllm_v1_adapter import (
         init_lmcache_engine as ascend_init_lmcache_engine,
