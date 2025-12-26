b6e3f20f9d941b35bd4641122d6540d0710501d2
Matthew Yeung <yyygggfff@hotmail.com>
2025-12-26 19:33:08 +0000

    use new with patch instead of side_effect

--- tests/v1/test_npu_connector.py
+++ tests/v1/test_npu_connector.py
@@ -2,7 +2,6 @@
 
 # Third Party
 import pytest
-import torch
 from unittest.mock import patch
 
 # First Party
@@ -25,14 +24,14 @@ from lmcache_ascend.v1.npu_connector import (
 def test_vllm_paged_connector_v2_with_npu_and_mla(use_npu, use_mla):
     target_patch = "lmcache_tests.v1.test_gpu_connector.VLLMPagedMemGPUConnectorV2"
     
-    with patch(target_patch, side_effect=VLLMPagedMemNPUConnectorV2):
+    with patch(target_patch, new=VLLMPagedMemNPUConnectorV2):
         original_test_vllm_paged_connector_v2_with_gpu_and_mla(use_npu, use_mla)
 
 @pytest.mark.parametrize("use_npu", [True])
 def test_layerwise_vllm_paged_connector_with_npu(use_npu):
     target_patch = "lmcache_tests.v1.test_gpu_connector.VLLMPagedMemLayerwiseGPUConnector"
     
-    with patch(target_patch, side_effect=VLLMPagedMemLayerwiseNPUConnector):
+    with patch(target_patch, new=VLLMPagedMemLayerwiseNPUConnector):
         original_test_layerwise_vllm_paged_connector_with_gpu(use_npu)
 
 
@@ -40,11 +39,11 @@ def test_layerwise_vllm_paged_connector_with_npu(use_npu):
 def test_batched_layerwise_vllm_paged_connector_with_npu(use_npu):
     target_patch = "lmcache_tests.v1.test_gpu_connector.VLLMPagedMemLayerwiseGPUConnector"
     
-    with patch(target_patch, side_effect=VLLMPagedMemLayerwiseNPUConnector):
+    with patch(target_patch, new=VLLMPagedMemLayerwiseNPUConnector):
         original_test_batched_layerwise_vllm_paged_connector_with_gpu(use_npu)
 
 def test_vllm_paged_connector_v2_to_npu_bench(benchmark):
     target_patch = "lmcache_tests.v1.test_gpu_connector.VLLMPagedMemGPUConnectorV2"
 
-    with patch(target_patch, side_effect=VLLMPagedMemNPUConnectorV2):
+    with patch(target_patch, new=VLLMPagedMemNPUConnectorV2):
         original_test_vllm_paged_connector_v2_to_gpu_bench(benchmark)
