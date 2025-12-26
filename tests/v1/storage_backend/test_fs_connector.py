b07ccc092005d9c29068ac3de32b20912c05efd1
Matthew Yeung <yyygggfff@hotmail.com>
2025-12-26 14:45:41 +0000

    WIP - added fs connector test from upstream

---
+++ tests/v1/storage_backend/test_fs_connector.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+# First Party
+from lmcache_tests.v1.storage_backend.test_fs_connector import (
+    temp_fs_path,
+    async_loop,
+    local_cpu_backend,
+    remote_backend_with_fs,
+    TestFSConnector
+)
