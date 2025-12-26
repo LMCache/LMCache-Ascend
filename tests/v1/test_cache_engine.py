59a31f2cfd94cf3e647b2d9a46821fb95e094e0f
Matthew Yeung <yyygggfff@hotmail.com>
2025-12-26 19:23:28 +0000

    tidy up test_cache_engine

--- tests/v1/test_cache_engine.py
+++ tests/v1/test_cache_engine.py
@@ -3,8 +3,6 @@
 import pytest
 import torch
 
-import lmcache_ascend
-
 from lmcache_tests.v1.test_cache_engine import (
     test_paged_same_retrieve_store,
     test_paged_retrieve_prefix as original_paged_retrieve_prefix,
@@ -19,18 +17,15 @@ from lmcache_tests.v1.test_cache_engine import (
     test_force_store_wait,
     test_builder_destroy,
     test_builder_destroy_multiple_instances,
-    # test_multi_device_backends, TODO (gingfung): once we supported NPUDirectFS, re-enable this.
+    # TODO (gingfung): once we supported NPUDirectFS, re-enable this.
+    # test_multi_device_backends, 
 )
 
-# TODO (gingfung): removed cachegen test untill ready
+# TODO (gingfung): removed cachegen test until ready
 @pytest.mark.parametrize("fmt", ["vllm"])
 @pytest.mark.parametrize("chunk_size", [128, 256])
 @pytest.mark.parametrize("backend", ["cpu", "local_disk", "remote"])
 @pytest.mark.parametrize("lmserver_v1_process", ["cpu"], indirect=True)
-@pytest.mark.skipif(
-    not torch.cuda.is_available(),
-    reason="TODO: Add non-CUDA implementation to VLLMPagedMemNPUConnectorV2",
-)
 def test_paged_retrieve_prefix_patched(
     fmt, chunk_size, backend, lmserver_v1_process, autorelease_v1
 ):
