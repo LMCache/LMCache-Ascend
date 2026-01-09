a0380fbbc1945773cebe64a5d777ee14795fc1de
Matthew Yeung <yyygggfff@hotmail.com>
2026-01-09 11:17:22 +0000

    WIP - add multiprocess custom types test from upstream

---
+++ tests/v1/multiprocess/test_custom_types.py
@@ -0,0 +1,6 @@
+from lmcache_tests.v1.multiprocess.test_custom_types import (
+    test_ipc_cache_engine_key_serialization,
+    test_cudaipc_wrapper_serialization,
+    test_cudaipc_wrapper_list_serialization,
+    test_cudaipc_wrapper_multiprocess_serialization,
+)
