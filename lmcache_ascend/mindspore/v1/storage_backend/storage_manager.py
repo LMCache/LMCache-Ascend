eb2c0e2924d2788fb61fa5a3a50c7b453c4ddc13
sunshine <xianiming1@huawei.com>
2026-01-20 10:09:52 +0800

    fix: linting and formatting issues

--- lmcache_ascend/mindspore/v1/storage_backend/storage_manager.py
+++ lmcache_ascend/mindspore/v1/storage_backend/storage_manager.py
@@ -1,51 +1,28 @@
 # SPDX-License-Identifier: Apache-2.0
 # Standard
 from collections import OrderedDict
-from concurrent.futures import Future
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Coroutine,
-    Generator,
-    List,
-    Optional,
-    Sequence,
-)
+from typing import TYPE_CHECKING, Optional, Sequence
 import asyncio
-import functools
 import threading
 
 # Third Party
-import torch
-
-# First Party
 from lmcache.config import LMCacheEngineMetadata
-from lmcache.logging import init_logger
-from lmcache.utils import (
-    CacheEngineKey,
-    _lmcache_nvtx_annotate,
-    start_loop_in_thread_with_exceptions,
-)
+from lmcache.utils import CacheEngineKey, start_loop_in_thread_with_exceptions
 from lmcache.v1.config import LMCacheEngineConfig
-from lmcache.v1.event_manager import EventManager, EventStatus, EventType
-from lmcache.v1.memory_management import (
-    MemoryFormat,
-    MemoryObj,
-)
+from lmcache.v1.event_manager import EventManager
+from lmcache.v1.memory_management import MemoryObj
 from lmcache.v1.storage_backend import CreateStorageBackends
 from lmcache.v1.storage_backend.abstract_backend import (
     AllocatorBackendInterface,
     StorageBackendInterface,
 )
-from lmcache.v1.storage_backend.local_cpu_backend import LocalCPUBackend
-from lmcache.v1.storage_backend.storage_manager import (
-    StorageManager,
-)
+import torch
 
+# First Party
 from lmcache_ascend.v1.npu_connector import is_310p
 
 if TYPE_CHECKING:
-    # First Party
+    # Third Party
     from lmcache.v1.cache_controller.worker import LMCacheWorker
     from lmcache.v1.lookup_client.lmcache_async_lookup_client import (
         LMCacheAsyncLookupServer,
@@ -89,7 +66,7 @@ def allocate_and_copy_objects_310p(
 
         if memory_obj is None or memory_obj.tensor is None:
             break
-        
+
         if is_310p():
             memory_obj.tensor.copy_(src_memory_obj.tensor, non_blocking=True)
         else:
@@ -97,13 +74,12 @@ def allocate_and_copy_objects_310p(
                 memory_obj.tensor.copy_(src_memory_obj.tensor, non_blocking=True)
         allocated_objects.append(memory_obj)
 
-    if is_310p():     
-        torch.cuda.synchronize()       
+    if is_310p():
+        torch.cuda.synchronize()
     else:
         stream.synchronize()
-    
-    return keys[: len(allocated_objects)], allocated_objects
 
+    return keys[: len(allocated_objects)], allocated_objects
 
 
 def StorageManager__init__(
@@ -113,7 +89,6 @@ def StorageManager__init__(
     event_manager: EventManager,
     lmcache_worker: Optional["LMCacheWorker"] = None,
 ):
-
     self.config = config
     self.metadata = metadata
     self.loop = asyncio.new_event_loop()
