329a033db8809c45e3aae1ec6afed6016388ed3d
chloroethylene <jjysama@gmail.com>
2026-01-27 06:14:02 +0000

    [feature] LMCache-Ascend 0.3.12 support mindspore

--- lmcache_ascend/integration/vllm/vllm_v1_adapter.py
+++ lmcache_ascend/integration/vllm/vllm_v1_adapter.py
@@ -19,24 +19,21 @@ from vllm.distributed.parallel_state import get_tp_group
 from vllm.utils import get_kv_cache_torch_dtype
 import torch
 
-# First Party
 from lmcache_ascend import _build_info
 
+# First Party
 if _build_info.__framework_name__ == "pytorch":
-    # First Party
     from lmcache_ascend.v1.npu_connector import (
         VLLMBufferLayerwiseNPUConnector,
         VLLMPagedMemLayerwiseNPUConnector,
         VLLMPagedMemNPUConnectorV2,
     )
 elif _build_info.__framework_name__ == "mindspore":
-    # First Party
     from lmcache_ascend.mindspore.v1.npu_connector import (
         VLLMBufferLayerwiseNPUConnector,
         VLLMPagedMemLayerwiseNPUConnector,
         VLLMPagedMemNPUConnectorV2,
     )
-
 
 logger = init_logger(__name__)
 
